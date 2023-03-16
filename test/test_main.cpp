4bed55e88e4de4d15f3d5b0869ef8ee03732a19d
tonted <yddetco@gmail.com>
2023-03-16 19:58:46 -0400

    :test_tube: Add test_main file with configuration!

---
+++ test/test_main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Teddy BLANCO on 2023-03-16.
+//
+#define DOCTEST_CONFIG_IMPLEMENT
+
+#include "test_header.hpp"
+
+int main(int argc, char** argv) {
+	doctest::Context context;
+
+	context.applyCommandLine(argc, argv);
+	context.setOption("no-breaks", true);
+
+
+	int res = context.run();
+	if (context.shouldExit())
+		return res;
+	return res;
+}
