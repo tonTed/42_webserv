459c5476075309f0f78244e6dd0a8dd3d3f6f5be
tonted <yddetco@gmail.com>
2023-03-17 10:05:34 -0400

    :sparkles: Create Request Class with first param!

--- src/Request/Request.cpp
+++ src/Request/Request.cpp
@@ -1,1 +1,35 @@
 #include "Request.hpp"
+#include "unistd.h"
+
+/**
+ * @brief Construct a new Request object
+ * @details Get the raw resources from the client
+ *
+ * @param client The client that sent the resources
+ */
+Request::Request(const int client) : _client(client) {
+	try {
+		try {
+			_getRawRequest();
+		} catch (std::runtime_error &e) {
+			throw e;
+			//TODO: send error to client
+		}
+	} catch (...) {}
+}
+
+
+/**
+ * @brief Get the raw resources from the client
+ * @details Read the client socket and store the resources in _rawRequest
+ *
+ * @question: can we have an error reading the socket?
+ */
+void Request::_getRawRequest() {
+	char	buffer[MAX_REQUEST_SIZE + 2];
+
+	if (read(_client, buffer, MAX_REQUEST_SIZE + 2) <= MAX_REQUEST_SIZE)
+		_rawRequest = buffer;
+	else
+		throw std::runtime_error(ERR_REQUEST_SIZE);
+}
--- src/Request/Request.hpp
+++ src/Request/Request.hpp
@@ -1,9 +1,35 @@
 #ifndef REQUEST_HPP
 #define REQUEST_HPP
 
+#include <string>
+#include <map>
+#include <sstream>
+#include <iostream>
+
+#include "../../includes/webserv.hpp"
+
+struct RequestLine {
+	eRequestType	_type;
+	std::string		_path;
+	std::string 	_version;
+};
 
 class Request {
+public:
+	Request(const int client);
+	~Request();
 
+private:
+	Request();
+
+	const int 							_client;
+
+	std::string							_rawRequest;
+	RequestLine							_requestLine;
+	std::map<std::string, std::string>	_headers;
+	std::string							_body;
+
+	void _getRawRequest();
 };
 
 
