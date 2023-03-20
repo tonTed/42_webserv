b5b44088d30bcfa9eb5369a18f0b24efc30a1c1a
tonted <yddetco@gmail.com>
2023-03-20 08:28:40 -0400

    🗑️ ours playground

--- includes/ours.hpp
+++ includes/ours.hpp
@@ -5,6 +5,11 @@
 #ifndef OURS_HPP
 # define OURS_HPP
 
+# define CRLF "\r\n"
+# define CR '\r'
+
+# define HTTP_VERSION "HTTP/1.1"
+
 //Server limits
 # define MAX_REQUEST_SIZE 4096
 
@@ -12,7 +17,22 @@
 //Exception messages
 # define ERR_REQUEST_SIZE "Request size is too big"
 # define ERR_SOCKET_READ "Error reading socket"
+# define ERR_START_LINE_NO_CRLF "Invalid start line: no CRLF"
+# define ERR_START_LINE_INVALID_METHOD "Invalid start line: invalid method"
+# define ERR_START_LINE_INVALID_VERSION "Invalid start line: invalid version"
+# define ERR_START_LINE_INVALID_LINE "Invalid start line: invalid line"
+# define ERR_HEADER_DUPLICATE "Invalid header: duplicate header"
+# define ERR_HEADER_MISSING_HEADER "Invalid header: missing header"
+# define ERR_HEADER_INVALID_VALUE "Invalid header: invalid value"
 
+/**
+ * @brief	Enum of all the possible request types.
+ * 			Used in the StartLine struct.
+ *
+ * @attention	Add new request types here,
+ * 				needs to add them in the _setMethod() method in Request.cpp
+ *
+ * */
 enum eRequestType
 {
 	GET,
@@ -23,6 +43,7 @@ enum eRequestType
 	CONNECT,
 	OPTIONS,
 	TRACE,
+	PATCH,
 	UNKNOWN
 };
 
--- oursPlayground/ours_main.cpp
+++ oursPlayground/ours_main.cpp
@@ -8,14 +8,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "../includes/webserv.hpp"
-
+#include <string.h>
+#include <errno.h>
+#include <fstream>
+#include <string>
 
 #define stringify( name ) #name
 
 int main() {
 
 
-	if (true) {
+	if (false) {
 
 		int fd = open("oursPlayground/test.http", O_RDONLY);
 		if (fd == -1)
@@ -34,7 +37,6 @@ int main() {
 		close(fd);
 	}
 
-
 	//Convert enum to string
 	if (false)
 	{
@@ -42,6 +44,43 @@ int main() {
 		std::cout << str << std::endl;
 	}
 
+	if (true)
+	{
+		char str[] = "GET www.google.com HTTP/1.1\r\n";
+		std::string line = str;
+		std::cout << strlen(str) << std::endl;
+		std::cout << line.length() << std::endl;
+
+		if (str[strlen(str) - 2] == '\r')
+			std::cout << "true" << std::endl;
+		else
+			std::cout << "false" << std::endl;
+
+		if (line.substr(line.length() - 2, 1) == "\r")
+			std::cout << "true" << std::endl;
+		else
+			std::cout << "false" << std::endl;
+
+		std::ifstream ifs("oursPlayground/test.http");
+		std::string line2;
+		std::getline(ifs, line2);
+		std::cout << line2 << std::endl;
+		std::cout << line2.length() << std::endl;
+		if (line2.length() > 1 || line2.substr(line2.length() - 1, 1) == "\r")
+			std::cout << "true" << std::endl;
+		else
+			std::cout << "false" << std::endl;
+
+		std::ifstream ifs2("test/resources/request_errFirstLine.http");
+		std::string line3;
+		std::getline(ifs2, line3);
+		std::cout << line3 << std::endl;
+		std::cout << line3.length() << std::endl;
+		if (line3.length() > 1 || line3.substr(line3.length() - 1, 1) == "\r")
+			std::cout << "true" << std::endl;
+		else
+			std::cout << "false" << std::endl;
+	}
 	return 0;
 
 }
