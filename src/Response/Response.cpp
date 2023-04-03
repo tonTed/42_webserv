e9b8e067244c7e89037b0255f59647d5b5db1b65
tblanco <tblanco@student.42quebec.com>
2023-04-03 17:21:03 -0400

    🏗Building Response

--- src/Response/Response.cpp
+++ src/Response/Response.cpp
@@ -1,1 +1,27 @@
 #include "Response.hpp"
+
+Response::Response(const Request &request, int status) : _request(request), _status(status){
+	_config = ConfigServer::getInstance();
+
+	if (status != 200) {
+		//send error
+	}
+	else {
+		//send file
+	}
+}
+
+void Response::sendError() {
+	std::string	path;
+	std::string errorPage;
+
+	path = resolvePath(_request._startLine.path);
+}
+
+std::string Response::resolvePath(const std::string &path) {
+	(void)path;
+
+	std::string resolvedPath = "Hello" ;
+
+	return resolvedPath;
+}
--- src/Response/Response.hpp
+++ src/Response/Response.hpp
@@ -1,9 +1,22 @@
 #ifndef RESPONSE_HPP
 #define RESPONSE_HPP
 
+#include "../../includes/webserv.hpp"
+#include "../Request/Request.hpp"
 
 class Response {
 
+public:
+	Response(const Request &request, const int status);
+
+	void 			sendError();
+
+	std::string	resolvePath(const std::string &path);
+
+public:
+		const Request & _request;
+		int 			_status;
+		ConfigServer	*_config;
 };
 
 
