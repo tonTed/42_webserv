1226bf8f489edb7e699e409597844c1b9cf709e3
tonted <yddetco@gmail.com>
2023-04-13 08:06:41 -0400

    🚧 RequestException:: Class what() method in public!

--- src/Request/RequestException.hpp
+++ src/Request/RequestException.hpp
@@ -11,24 +11,28 @@ class RequestException {
 
 public:
 	class ReadError : public std::exception {
+	public:
 		const char * what() const throw() {
 			return ERR_SOCKET_READ;
 		}
 	};
 
 	class MaxSize : public std::exception {
+	public:
 		const char * what() const throw() {
 			return ERR_REQUEST_SIZE;
 		}
 	};
 
 	class NoCRLF : public std::exception {
+	public:
 		const char * what() const throw() {
 			return ERR_NO_CRLF;
 		}
 	};
 
 	class InvalidLine : public std::exception {
+	public:
 		const char * what() const throw() {
 			return ERR_INVALID_LINE;
 		}
@@ -38,24 +42,28 @@ public:
 	public:
 
 		class InvalidMethod : public std::exception {
+		public:
 			const char * what() const throw() {
 				return ERR_START_LINE_INVALID_METHOD;
 			}
 		};
 
 		class InvalidVersion : public std::exception {
+		public:
 			const char * what() const throw() {
 				return ERR_START_LINE_INVALID_VERSION;
 			}
 		};
 
 		class NotAllowedMethod : public std::exception {
+		public:
 			const char * what() const throw() {
 				return ERR_START_LINE_NOT_ALLOWED_METHOD;
 			}
 		};
 
 		class InvalidURI : public std::exception {
+		public:
 			const char * what() const throw() {
 				return ERR_START_LINE_INVALID_URI;
 			}
@@ -66,24 +74,28 @@ public:
 	class Header {
 	public:
 		class DuplicateKey : public std::exception {
+		public:
 			const char * what() const throw() {
 				return ERR_HEADER_DUPLICATE;
 			}
 		};
 
 		class MissingHeader : public std::exception {
+		public:
 			const char * what() const throw() {
 				return ERR_HEADER_MISSING_HEADER;
 			}
 		};
 
 		class InvalidValue : public std::exception {
+		public:
 			const char * what() const throw() {
 				return ERR_HEADER_INVALID_VALUE;
 			}
 		};
 
 		class InvalidKey : public std::exception {
+		public:
 			const char * what() const throw() {
 				return ERR_HEADER_INVALID_KEY;
 			}
