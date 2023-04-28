b22d12d1586ad10cf7baa9ddfc944eb5653ff4a5
tblanco <tblanco@student.42quebec.com>
2023-04-28 10:31:36 -0400

    🐛 fix allowed methods

--- src/PathResolver/PathResolver.hpp
+++ src/PathResolver/PathResolver.hpp
@@ -17,6 +17,8 @@ public:
 	bool			hasExtension(const std::string &path);
 	bool 			isRootValid();
 	void 			addIndex(bool hasLocation, std::string location);
+	
+	bool			isAllowedMethod(std::vector<enum eRequestType> vect);
 
 	Request &		_request;
 	ServerData		_serverData;
